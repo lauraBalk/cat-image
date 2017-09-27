9321c7d18d065dbc6f80850642578ae8c13ab631
lauraBalk <laura.balk2@gmail.com>
2017-09-27 13:53:35 +0200

    change

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
 	<body>
 		<?php 
 		$url =  "http://thecatapi.com/api/images/get?api_key=MjI4NDk4&category=space&results_per_page=1";
-		$raw = file_get_contents($url);
-		echo "<img class='cat' src=".$url."/>";
+		echo "<img class='cat' src='".$url."'/>";
 		?>
 	</body>
 </html>
